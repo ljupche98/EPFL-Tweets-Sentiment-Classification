ab2cf95c53ef8d6f29493550140255f7937faf65
korunosk <mladen.korunoski@epfl.ch>
2019-12-02 15:30:41 +0100

    Fix tokenizer

--- src/tweets/utils/preprocessing.py
+++ src/tweets/utils/preprocessing.py
@@ -30,21 +30,24 @@ class TweetPreprocessor:
         def re_sub(pattern, repl):
             return re.sub(pattern, repl, text, flags=TweetPreprocessor.FLAGS)
 
-        text = re_sub(r"https?:\/\/\S+\b|www\.(\w+\.)+\S*", "<url>")
-        text = re_sub(r"@\w+", "<user>")
-        text = re_sub(r"{}{}[)dD]+|[)dD]+{}{}".format(eyes, nose, nose, eyes), "<smile>")
-        text = re_sub(r"{}{}p+".format(eyes, nose), "<lolface>")
-        text = re_sub(r"{}{}\(+|\)+{}{}".format(eyes, nose, nose, eyes), "<sadface>")
-        text = re_sub(r"{}{}[\/|l*]".format(eyes, nose), "<neutralface>")
+        text = re_sub(r"https?:\/\/\S+\b|www\.(\w+\.)+\S*", " <url> ")
+        text = re_sub(r"@\w+", " <user> ")
+        text = re_sub(r"{}{}[)dD]+|[)dD]+{}{}".format(eyes, nose, nose, eyes), " <smile> ")
+        text = re_sub(r"{}{}p+".format(eyes, nose), " <lolface> ")
+        text = re_sub(r"{}{}\(+|\)+{}{}".format(eyes, nose, nose, eyes), " <sadface> ")
+        text = re_sub(r"{}{}[\/|l*]".format(eyes, nose), " <neutralface> ")
         text = re_sub(r"/"," / ")
         text = re_sub(r"<3","<heart>")
-        text = re_sub(r"[-+]?[.\d]*[\d]+[:,.\d]*", "<number>")
+        text = re_sub(r"[-+]?[.\d]*[\d]+[:,.\d]*", " <number> ")
         text = re_sub(r"#\S+", TweetPreprocessor.hashtag)
-        text = re_sub(r"([!?.]){2,}", r"\1 <repeat>")
-        text = re_sub(r"\b(\S*?)(.)\2{2,}\b", r"\1\2 <elong>")
+        text = re_sub(r"([!?.]){2,}", r"\1 <repeat> ")
+        text = re_sub(r"\b(\S*?)(.)\2{2,}\b", r"\1\2 <elong> ")
 
         ## -- I just don't understand why the Ruby script adds <allcaps> to everything so I limited the selection.
         # text = re_sub(r"([^a-z0-9()<>'`\-]){2,}", TweetPreprocessor.allcaps)
         text = re_sub(r"([A-Z]){2,}", TweetPreprocessor.allcaps)
 
-        return text.lower()
+        text = re_sub(r"'", "")
+        text = re_sub("\s+", " ")
+
+        return text.lower().strip()
