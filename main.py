665c546a76df53d95d8d3e4e9e2f56ddb4a87ac6
korunosk <mladen.korunoski@epfl.ch>
2019-12-02 22:47:29 +0100

    Update main

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from src.serializer import *
+import numpy as np
+np.set_printoptions(suppress=True)
 
-s = DataSerializer()
+# from src.serializer import DataSerializer
+# s = DataSerializer()
 # s.save_generators('bow')
-# s.save_generators('tf_idf')
+# s.save_generators('tf_idf', 'word')
+# s.save_generators('tf_idf', 'ngram')
+# s.save_generators('tf_idf', 'char')
 # s.save_words('glove')
-s.save_words('fasttext')
+# s.save_words('fasttext')
 # s.save_sentences()
-
 
 # from src.tweets.model import TweetsModel
 # from src.representations.generator import RepresentationsGenerator
-
 # tweets_model = TweetsModel()
 # representations_generator = RepresentationsGenerator(tweets_model)
+# representations_generator.sent2vec(dim=25)
 
-# representations_generator.fasttext(dim=25)
-
+from src.experiments.executor import ExperimentExecutor
+from src.models.classic import LogisticRegression
+ExperimentExecutor.execute(LogisticRegression)
