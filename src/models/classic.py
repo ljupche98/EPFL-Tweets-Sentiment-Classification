770e9dc4756846bbe27f6261580eea7476bfbf17
korunosk <mladen.korunoski@epfl.ch>
2019-12-06 01:12:31 +0100

    Update classic ML models

--- src/models/classic.py
+++ src/models/classic.py
@@ -7,24 +7,21 @@ with the following signature:
     Model(X_train, X_test, y_train, y_test)
 that does proper grid search over the hyperparameter space and
 cross-validates the parameters using 5 folds.
-
-TODO: Store the results in a file.
 '''
 import numpy as np
+import pickle
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
 
-def LogisticRegression(X_train, X_test, y_train, y_test):
-    parameters = {
-        'alpha': np.linspace(0.01, 1, 10)
-    }
-    clf = GridSearchCV(RidgeClassifier(),
-                       parameters,
-                       cv=5)
-    clf.fit(X_train, y_train)
+MODELS_DIR = 'data/models/'
+CV = 5
+
+
+def print_stats(clf, data):
+    X_train, X_test, y_train, y_test = data
     print('Optimal parameters:')
     print('', clf.best_params_)
     print('Score:')
@@ -32,36 +29,64 @@ def LogisticRegression(X_train, X_test, y_train, y_test):
     print(f'  Test set: {round(clf.score(X_test, y_test) * 100, 2)}%')
 
 
-def SVM(X_train, X_test, y_train, y_test):
-    parameters = {
-        'alpha': np.linspace(0.01, 1, 10)
-    }
-    clf = GridSearchCV(SGDClassifier(loss='hinge',
-                                     penalty='l2'),
-                       parameters,
-                       cv=5)
-    clf.fit(X_train, y_train)
-    print('Optimal parameters:')
-    print('', clf.best_params_)
-    print('Score:')
-    print(f' Train set: {round(clf.score(X_train, y_train) * 100, 2)}%')
-    print(f'  Test set: {round(clf.score(X_test, y_test) * 100, 2)}%')
+def persist_model(clf, mname):
+    with open(MODELS_DIR + mname, 'wb') as file:
+        pickle.dump(clf, file)
 
 
-def RandomForest(X_train, X_test, y_train, y_test):
-    parameters = { }
-    clf = GridSearchCV(RandomForestClassifier(n_estimators=100,
-                                              max_depth=2),
-                       parameters,
-                       cv=5)
-    clf.fit(X_train, y_train)
-    print('Optimal parameters:')
-    print('', clf.best_params_)
-    print('Score:')
-    print(f' Train set: {round(clf.score(X_train, y_train) * 100, 2)}%')
-    print(f'  Test set: {round(clf.score(X_test, y_test) * 100, 2)}%')
+def load_model(mname):
+    with open(MODELS_DIR + mname, 'rb') as file:
+        return pickle.load(file)
 
 
-def NaiveBayes(X_train, X_test, y_train, y_test):
+def LogisticRegression(X_train, X_test, y_train, y_test, load=False):
+    mname = 'LogisticRegression'
+    if load:
+        clf = load_model(mname)
+    else:
+        parameters = {
+            'alpha': np.linspace(0.01, 1, 10)
+        }
+        clf = GridSearchCV(RidgeClassifier(),
+                        parameters,
+                        cv=CV)
+        clf.fit(X_train, y_train)
+        persist_model(clf, mname)
+    print_stats(clf, (X_train, X_test, y_train, y_test))
+
+
+def SVM(X_train, X_test, y_train, y_test, load=False):
+    mname = 'SVM'
+    if load:
+        clf = load_model(mname)
+    else:
+        parameters = {
+            'alpha': np.linspace(0.01, 1, 10)
+        }
+        clf = GridSearchCV(SGDClassifier(loss='hinge',
+                                        penalty='l2'),
+                        parameters,
+                        cv=CV)
+        clf.fit(X_train, y_train)
+        persist_model(clf, mname)
+    print_stats(clf, (X_train, X_test, y_train, y_test))
+
+
+def RandomForest(X_train, X_test, y_train, y_test, load=False):
+    mname = 'RandomForest'
+    if load:
+        clf = load_model(mname)
+    else:
+        parameters = { }
+        clf = GridSearchCV(RandomForestClassifier(n_estimators=100,
+                                                max_depth=2),
+                        parameters,
+                        cv=CV)
+        clf.fit(X_train, y_train)
+        persist_model(clf, mname)
+    print_stats(clf, (X_train, X_test, y_train, y_test))
+
+
+def NaiveBayes(X_train, X_test, y_train, y_test, load=False):
     # TODO: Implement
     pass
