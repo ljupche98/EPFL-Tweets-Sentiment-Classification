a09511bd9434b0d04b1c09bcf38aaf759d766f20
korunosk <mladen.korunoski@epfl.ch>
2019-12-17 15:29:41 +0100

    Update classic ML

--- src/models/classic.py
+++ src/models/classic.py
@@ -19,6 +19,7 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import MinMaxScaler
 
 MODELS_DIR = 'data/models/'
 CV = 5
@@ -85,11 +86,11 @@ def RandomForest(X_train, X_test, y_train, y_test, load=False):
         clf = load_model(mname)
     else:
         parameters = {
-                'min_samples_leaf': [3,5,25,50]
-                'n_etimators' : [25, 100, 200]
-                'max_features' : ['sqrt', 'log2']
+            'min_samples_leaf': [3, 5, 25, 50],
+            'n_estimators': [25, 100, 200],
+            'max_features': ['sqrt', 'log2']
         }
-        clf = GridSearchCV(RandomForestClassifier( bootstrap=True, n_jobs=-1, random_state=50),
+        clf = GridSearchCV(RandomForestClassifier(bootstrap=True, n_jobs=-1, random_state=50),
                            parameters,
                            cv=CV)
         clf.fit(X_train, y_train)
@@ -98,30 +99,20 @@ def RandomForest(X_train, X_test, y_train, y_test, load=False):
 
 
 def NaiveBayes(X_train, X_test, y_train, y_test, load=False):
-    # TODO: Implement
-     mname = 'NaiveBayes'
+    mname = 'NaiveBayes'
     if load:
         clf = load_model(mname)
     else:
-        if np.any(X_train<0):
-            parameters = {
-                'var_smoothing':[1e-5,1e-7,1e-9,1e-11,1e-13]
-            }
-            clf = GridSearchCV(GaussianNB(),
-                               parameters,
-                               cv=CV)
-            clf.fit(X_train, y_train)
-            # persist_model(clf, mname)
-        else:
-            parameters = {
-                'alpha':np.linspace(0.0001,1,101)
-            }
-            clf = GridSearchCV(MultinomialNB(),
-                               parameters,
-                               cv=CV)
-            clf.fit(X_train, y_train)
-            # persist_model(clf, mname)
-            
+        if type(X_train) == np.ndarray:
+            scaler = MinMaxScaler().fit(X_train)
+            X_train = scaler.transform(X_train)
+            X_test = scaler.transform(X_test)
+        parameters = {
+            'alpha': np.linspace(0.0001, 1, 101)
+        }
+        clf = GridSearchCV(MultinomialNB(),
+                           parameters,
+                           cv=CV)
+        clf.fit(X_train, y_train)
+        persist_model(clf, mname)
     print_stats(clf, (X_train, X_test, y_train, y_test))
-    
-    pass
